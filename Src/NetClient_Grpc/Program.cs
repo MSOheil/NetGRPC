95bf9dc9028cceee10cfcfb1089408f129ca997f
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-03 14:35:57 +0330

    Sent request to grpc and tested it

--- Src/NetClient_Grpc/Program.cs
+++ Src/NetClient_Grpc/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using NetGrpc;
+
 Console.WriteLine("Hello, World!");
+
+var input = new HelloRequest
+{
+    Name = "MSoheil",
+};
+
+var channel = GrpcChannel.ForAddress("https://localhost:7037");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(input);
+
+
+
+Console.WriteLine(reply.Message);
+
+Console.ReadLine();
