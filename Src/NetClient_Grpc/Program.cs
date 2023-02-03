afbeb5021e75018299e7370a64ba4881ee8467c0
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-03 15:24:40 +0330

    Added new proto and is called with client project and tested it

--- Src/NetClient_Grpc/Program.cs
+++ Src/NetClient_Grpc/Program.cs
@@ -1,21 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Grpc.Net.Client;
 using NetGrpc;
+using NetGrpc.Protos;
 
 Console.WriteLine("Hello, World!");
 
-var input = new HelloRequest
-{
-    Name = "MSoheil",
-};
+//var input = new HelloRequest
+//{
+//    Name = "MSoheil",
+//};
+
+//var channel = GrpcChannel.ForAddress("https://localhost:7037");
+//var client = new Greeter.GreeterClient(channel);
+
+//var reply = await client.SayHelloAsync(input);
 
 var channel = GrpcChannel.ForAddress("https://localhost:7037");
-var client = new Greeter.GreeterClient(channel);
+var customerClient = new Customer.CustomerClient(channel);
 
-var reply = await client.SayHelloAsync(input);
+var clientReuqest = new CustomerLookUpModel
+{
+    UserId = 1,
+};
 
+var reply = await customerClient.GetCustomerInfoAsync(clientReuqest);
 
-
-Console.WriteLine(reply.Message);
-
+Console.WriteLine("Response from server is " + reply.FirstName + "last name is " + reply.LastName);
 Console.ReadLine();
---
+++ Src/NetGrpc/Services/CustomerService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using NetGrpc.Protos;
+
+namespace NetGrpc.Services;
+
+public class CustomerService : Customer.CustomerBase
+{
+    private readonly ILogger<CustomerService> logger;
+    public CustomerService(ILogger<CustomerService> logger)
+    {
+        this.logger = logger;
+    }
+    public override async Task<CustomerModel> GetCustomerInfo(CustomerLookUpModel request, ServerCallContext context)
+    {
+        CustomerModel outPut = new CustomerModel();
+        if (request.UserId.Equals(1))
+        {
+            outPut.FirstName = "MSoheil";
+            outPut.LastName = "Davoodi";
+        }
+        else if (request.UserId.Equals(2))
+        {
+            outPut.FirstName = "Amin";
+            outPut.LastName = "Esma";
+        }
+        else
+        {
+            outPut.FirstName = "MohammadReza";
+            outPut.LastName = "Savad";
+        }
+
+        return await Task.FromResult(outPut);
+    }
+}
+
